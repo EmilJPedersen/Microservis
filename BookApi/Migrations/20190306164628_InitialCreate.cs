d84b47972854ee7c6e0e8e8cedcbb21bec354a81
Tiffanatic <madsrynord@live.dk>
2019-03-06 17:54:50 +0100

    Book Api
    OBS vær opmærksom på connection string

---
+++ BookApi/Migrations/20190306164628_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    titel = table.Column<string>(nullable: true),
+                    imagePath = table.Column<string>(nullable: true),
+                    pages = table.Column<int>(nullable: false),
+                    desc = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
---
+++ BookApi/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApi.Models
+{
+    public class Book
+    {
+        
+        public int Id { get; set; }
+        public string titel { get; set; }
+        public string imagePath { get; set; }
+        public int pages { get; set; }
+        public string desc { get; set; }
+
+    }
+}
