45b3ce0e16f5d4a41b0f2795188b909ac1c5dfa3
Chrischlor <34272915+Chrischlor@users.noreply.github.com>
2019-03-07 09:38:38 +0100

    movie api
    Movie api

---
+++ MovieApi/MovieApi/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApi.Models
+{
+    public class Movie
+    {
+      
+       public int id { get; set; }
+
+        string Titel { get; set; }
+        string imagePath { get; set; }
+        string releaseDate { get; set; }
+        string Desc { get; set; }
+
+    }
+}
