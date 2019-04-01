9b9655be9c39e0e8e4198fc2a1529f0b87bba4f7
Runebjepsen <34272890+Runebjepsen@users.noreply.github.com>
2019-04-01 23:02:06 +0200

    ff

--- MicroLibrary/projects/movies-lib/src/lib/movies-lib.service.ts
+++ MicroLibrary/projects/movies-lib/src/lib/movies-lib.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { movie } from 'projects/Shared/movie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesLibService {
+  formData : movie;
+  readonly rootURL = "https://localhost:44350/api"
 
   constructor(private  http: HttpClient) { }
 
-getMovies(){
-  return this.http.get('the same thing here (Movies)');
-}
+  postBook(formData : movie){
+    return this.http.post(this.rootURL+'/movie',formData);
+  }
 
 }
