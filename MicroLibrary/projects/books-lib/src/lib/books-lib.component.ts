4fe4bac460c9921c3ed1bb53e7b0627bde2cf9f3
Runebjepsen <34272890+Runebjepsen@users.noreply.github.com>
2019-04-01 21:34:51 +0200

    færdig?

--- MicroLibrary/projects/books-lib/src/lib/books-lib.component.ts
+++ MicroLibrary/projects/books-lib/src/lib/books-lib.component.ts
@@ -2,26 +2,42 @@ import { Component, OnInit,NgModule } from '@angular/core';
 import { DataService } from  '../../../../src/app/data.service';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgForm } from '@angular/forms';
+import { BooksLibService } from "./books-lib.service";
+
 @Component({
   selector: 'lib-books-lib',
   templateUrl: './books-lib.component.html',
   styleUrls: ['./books-lib.component.css']
 })
 export class BooksLibComponent implements OnInit {
-books:object; 
-  constructor(private data: DataService) { }
+  constructor(private service : BooksLibService) { }
 
   ngOnInit() {
-    this.data.getBooks().subscribe(data=>{
-      this.books = data;
-      console.log(this.books);
-    })
+    this.resetForm();
   }
-  getBook(id):void{
-    this.data.getSpecificBook(id).subscribe(data=>{
-      this.books=data;
-      console.log(this.books);
-      })
+  resetForm(form? : NgForm){
+    if(form != null){
+      form.resetForm();
+    }    
+    this.service.formData = {
+      id : null,
+      Titel : '',
+      ImagePath : '',
+      pages : null,
+      desc : ''
+      
     }
+
+
+  }
+  onSubmit(form : NgForm){
+    this.insertRecord(form);
+  }
+  insertRecord(form : NgForm){
+     this.service.postBook(form.value).subscribe(res => {
+       this.resetForm(form);
+     })
+  }
 
 }
--- MicroLibrary/projects/books-lib/src/lib/books-lib.module.ts
+++ MicroLibrary/projects/books-lib/src/lib/books-lib.module.ts
@@ -1,14 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BooksLibComponent } from './books-lib.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from "@angular/forms";
+
+
+import { BooksLibComponent } from './books-lib.component';
+
 import { browser } from 'protractor';
+
 @NgModule({
-  declarations: [BooksLibComponent],
+  declarations: [
+    BooksLibComponent
+  ],
+  
   imports: [
    BrowserModule,
-   HttpClientModule 
+   HttpClientModule,
+   FormsModule
   ],
-  exports: [BooksLibComponent]
+  
+  exports: [
+    BooksLibComponent
+  ]
 })
 export class BooksLibModule { }
