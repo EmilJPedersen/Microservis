247fac6ba44b1c42d8f0333ed1443b18415ca236
Yusuf Yücel Akdag <yusu0166@edu.eal.dk>
2019-04-16 14:54:19 +0200

    Rabbitmq

---
+++ Rabbitmq/Send/Send.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using EasyNetQ;
+
+namespace Send
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", Port=5672, UserName="guest", Password="guest"};
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+                Console.WriteLine("Skriv den besked du gerne vil sende:");
+                string message = Console.ReadLine();
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+
+        }
+    }
+}
